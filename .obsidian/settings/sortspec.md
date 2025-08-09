65115747156add2056f5cbd533cf1716e4d7c8d4
thekenwhite <217611309+thekenwhite@users.noreply.github.com>
2025-08-09 19:22:10 +0900

    commit

---
+++ .obsidian/settings/sortspec.md
@@ -0,0 +1,10 @@
+---
+aliases: 
+cssclasses: 
+tags: 
+sorting-spec: |-
+  target-folder: /*
+  /:files
+  /folders
+  < vsc-unicode
+---
---
+++ config.sh
@@ -0,0 +1,5 @@
+
+git config user.name thekenwhite
+git config user.email 217611309+thekenwhite@users.noreply.github.com
+
+git config --global core.safecrlf false
