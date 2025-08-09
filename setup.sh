66b3fe048431828c9810cd1610d65daf1ab020aa
thekenwhite <217611309+thekenwhite@users.noreply.github.com>
2025-08-10 04:13:07 +0900

    commit

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 cd ..
 
 git clone https://github.com/thekenwhite/system
+git clone https://github.com/thekenwhite/server
+git clone https://github.com/thekenwhite/static
+git clone https://github.com/thekenwhite/editor
+git clone https://github.com/thekenwhite/target
